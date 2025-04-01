ecefb40e4207e9e9ab2d326624ea65d3daded54f
Manighaffari <manimicro2005@gmail.com>
2025-04-01 21:14:06 +0330

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -4,27 +4,18 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace پیک_نوروزی_سوال_1
+namespace پیک_نوروزی_8
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            int y, m, d, r, r1, r2;
-            y = int.Parse(Console.ReadLine());
-            m = int.Parse(Console.ReadLine());
-            d = int.Parse(Console.ReadLine());
-            r1 = (1404 - y - 1) * 365;
-            if (m <= 6)
-                r2 = 365 - ((m - 1) * 31) + d;
-            else
-                r2 = 365 - (186 + (m - 7) * 30 + d);
-            r = r1 + r2;
-            Console.WriteLine(r);
-            y = r / 365;
-            m = (r % 365) / 30;
-            d = ((r % 365) % 30);
-            Console.WriteLine("y = "+y+" "+"m"+m+"" +"d" + d);
+            int a, b, z;
+            a = int.Parse(Console.ReadLine());
+            b = int.Parse(Console.ReadLine());
+            for (z = a + 1; z <= b - 1; z++)
+            if (z % 5 == 0)
+                Console.WriteLine(z);
             Console.ReadLine();
         }
     }
